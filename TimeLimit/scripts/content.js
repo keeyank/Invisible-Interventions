15f6a7166b104a23934af07299db232c21ec79d5
Keeyan Kazemi <kazemikeeyan@gmail.com>
2023-06-22 14:33:08 -0700

    First commit - Time limits intervention

---
+++ TimeLimit/scripts/content.js
@@ -0,0 +1,8 @@
+// Create a new div element
+const div = document.createElement('div');
+
+// Set the content of the div
+div.innerHTML = 'This is an injected div!';
+
+// Inject the div into the document body
+document.body.appendChild(div);
