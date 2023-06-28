43baa558363238cec85e0bdfff57229d9b83c9de
Keeyan Kazemi <kazemikeeyan@gmail.com>
2023-06-28 15:43:12 -0700

    Set up typescript support

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This repository is for creating productivity extensions for social media applications that users can install in order to curb their excessive social media use. The extensions will additionally be used to conduct research on various interventions and their effects on peoples habitual consumption of social media.
+
+TimeLimit typescript setup
+- Must have NPM and typescript installed
+- Run npm install to get the chrome type
+- In the terminal, run tsc -w to turn on watch mode (transpiles automatically whenever you save the type script)
--- TimeLimit/scripts/content.js
+++ TimeLimit/scripts/content.js
@@ -2,59 +2,45 @@
 const dialogBoxContainer = document.createElement('div');
 dialogBoxContainer.id = 'dialogBoxContainer';
 document.body.appendChild(dialogBoxContainer);
-
 // Create the timer box container
 const timerBoxContainer = document.createElement('div');
 timerBoxContainer.id = 'timerBoxContainer';
-
 // Append it to the search bar
 const searchBar = document.querySelector('._ab18._ab1b');
 searchBar.appendChild(timerBoxContainer);
-
 let timeInMins = 0;
-
 // Load the dialog-1.html file into the dialog box container
 fetch(chrome.runtime.getURL('html/dialog-1.html'))
     .then(response => response.text())
     .then(html => {
-        dialogBoxContainer.innerHTML = html;
-
-        // Get the necessary elements
-        const dialogBox = document.getElementById('dialogBox');
-        const submitBtn = document.getElementById('submitBtn');
-
-        // Function to close the dialog box
-        function closeDialog() {
-            dialogBox.style.display = 'none';
-        }
-
-        submitBtn.addEventListener('click', function() {
-            // Obtain the time limit value
-            const inputElement = document.getElementById('timeLimit');
-            timeInMins = inputElement.value !== '' ? inputElement.value : inputElement.placeholder;
-
-            closeDialog();
-        });
+    dialogBoxContainer.innerHTML = html;
+    // Get the necessary elements
+    const dialogBox = document.getElementById('dialogBox');
+    const submitBtn = document.getElementById('submitBtn');
+    // Function to close the dialog box
+    function closeDialog() {
+        dialogBox.style.display = 'none';
+    }
+    submitBtn.addEventListener('click', function () {
+        // Obtain the time limit value
+        const inputElement = document.getElementById('timeLimit');
+        timeInMins = +(inputElement.value !== '' ? inputElement.value : inputElement.placeholder);
+        closeDialog();
     });
-
+});
 fetch(chrome.runtime.getURL('html/timer.html'))
     .then(response => response.text())
     .then(html => {
-        timerBoxContainer.innerHTML = html;
-
-        /*
-        Have a countdown timer that counts down from the time limit value
-        in both minutes and seconds. 
-        This countdown timer will also update the countdown timer html element
-        */
-
-        // Get the necessary elements
-        const timerMinutes = document.getElementById('minutes');
-
-        remainingTime = new Date()
-        remainingTime.setMinutes(timeInMins);
-        console.log(remainingTime);
-        
-        //timerMinutes.textContent = remainingTime.getMinutes();
-    });
-    
+    timerBoxContainer.innerHTML = html;
+    /*
+    Have a countdown timer that counts down from the time limit value
+    in both minutes and seconds.
+    This countdown timer will also update the countdown timer html element
+    */
+    // Get the necessary elements
+    const timerMinutes = document.getElementById('minutes');
+    // remainingTime = new Date()
+    // remainingTime.setMinutes(timeInMins);
+    // console.log(remainingTime);
+    //timerMinutes.textContent = remainingTime.getMinutes();
+});
