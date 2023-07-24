c6b2056955723c6f1f50e946e88cc5a5e545bc7b
Keeyan Kazemi <kazemikeeyan@gmail.com>
2023-07-24 11:32:26 -0700

    Added support for SQLlite - still needs work

--- server/apps/tracking/main.py
+++ server/apps/tracking/main.py
@@ -3,13 +3,27 @@ import time
 from typing import List
 from pydantic import BaseModel
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
+from . import crud, models, schemas
+from .database import SessionLocal, engine
 
-from apps.tracking.models.events import EventForm, EventModel, Events
+from sqlalchemy.orm import Session
+
+# from apps.tracking.models.events import EventForm, EventModel, Events
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 origins = ["*"]
 
@@ -26,21 +40,51 @@ app.add_middleware(
 async def get_status():
     return {"status": True}
 
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
-@app.get("/data")
-async def get_all_data_from_db() -> List[EventModel]:
-    events = Events.get_all_event_data()
-    return {"status": True, "result": events}
 
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
-@app.post("/data")
-async def append_data_to_db(form_data: EventForm) -> bool:
-    if form_data:
-        event = Events.insert_event_data(form_data)
+# @app.get("/data")
+# async def get_all_data_from_db() -> List[EventModel]:
+#     events = Events.get_all_event_data()
+#     return {"status": True, "result": events}
 
-        if event:
-            return {"status": True, "result": event}
-        else:
-            return {"status": False, "error": "MongoDB Error"}
-    else:
-        return {"status": False, "error": "Empty Form"}
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
+# def create_item_for_user(
+#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+# ):
+#     return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+# @app.get("/items/", response_model=list[schemas.Item])
+# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     items = crud.get_items(db, skip=skip, limit=limit)
+#     return items
+
+# @app.post("/data")
+# async def append_data_to_db(form_data: EventForm) -> bool:
+#     if form_data:
+#         event = Events.insert_event_data(form_data)
+
+#         if event:
+#             return {"status": True, "result": event}
+#         else:
+#             return {"status": False, "error": "MongoDB Error"}
+#     else:
+#         return {"status": False, "error": "Empty Form"}
+    
