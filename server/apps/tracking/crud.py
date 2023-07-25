8ded380f2c04489e1a99ddf3196f8594efe7b5f4
Keeyan Kazemi <kazemikeeyan@gmail.com>
2023-07-25 15:46:12 -0700

    Update rows when in same session

--- server/apps/tracking/crud.py
+++ server/apps/tracking/crud.py
@@ -1,8 +1,12 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
+from sqlalchemy import update
+
+from datetime import datetime, timedelta
 
 from . import models, schemas
 
+END_SESSION_SECONDS = 15
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -29,35 +33,44 @@ def create_user(db: Session, user: schemas.UserCreate):
 def session_update(db: Session, usage: schemas.UsageCreate):
 
     result = db.execute(text(f'''
-                        SELECT MAX(session_end)
+                        SELECT id, MAX(session_end) AS max_date
                         FROM Usage
                         WHERE user_id = {usage.user_id}
                         '''))
-    # DEBUG
     # Note: If the query does not return a value (i.e., no user_id rows yet, first time
     # gathering data for a user), query_objs has one None value
-    largest_date = result.scalars().all()[0]
-    print(largest_date)
-    print(type(largest_date))
+    row = result.one()
 
-    # TODO: Now that we have largest_date, we must use it to implement the logic outlined above
-    # We'll need to execute another raw SQL query, this time updating. We could also use ORM
-    
-    db_usage = models.Usage(user_id = usage.user_id, 
-                            session_begin = usage.session_begin, 
-                            session_end = usage.session_end)
-    db.add(db_usage)
-    db.commit()
-    db.refresh(db_usage)
-    return db_usage
+    # DEBUG
+    print(row.max_date)
+    print(row.id)
+
+    dt_format = '%Y-%m-%d %H:%M:%S.%f'
+    largest_dt = datetime.strptime(row.max_date, dt_format)
+    current_dt = usage.session_end  
+
+    if largest_dt != None and current_dt - largest_dt <= timedelta(seconds=END_SESSION_SECONDS):
+
+        # TODO: This works, but figure out why the server is throwing an exception
+        stmt = (update(models.Usage)
+             .values({"session_end": current_dt})
+             .where(models.Usage.id == row.id))
+        db.execute(stmt)
+        db.commit()
+        
+        # db.refresh(row)
+        print('done')
+
+        return row
+    else:
+        db_usage = models.Usage(user_id = usage.user_id, 
+                                session_begin = usage.session_begin, 
+                                session_end = usage.session_end)
+        db.add(db_usage)
+        db.commit()
+        db.refresh(db_usage)
+
+        return db_usage
 
 def get_usage(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Usage).offset(skip).limit(limit).all()
-
-
-# def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-#     db_item = models.Item(**item.dict(), owner_id=user_id)
-#     db.add(db_item)
-#     db.commit()
-#     db.refresh(db_item)
-#     return db_item
