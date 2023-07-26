a68113b32e15b54d56fdd8cdb7be8e94ce70ad83
Keeyan Kazemi <kazemikeeyan@gmail.com>
2023-07-26 12:11:28 -0700

    Sending http request from client to server

--- extensions/force-login/scripts/content.js
+++ extensions/force-login/scripts/content.js
@@ -1,3 +1,21 @@
+// HTTP Requests
+fetch('http://127.0.0.1:8000/usage/?user_id=1', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'text/plain'
+  },
+  body: JSON.stringify({})
+})
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error(error);
+  });
+
 var port = chrome.runtime.connect({name: "clear_cache"});
 // This isn't great - if the browsing data is removed while they are attempting to
 // log in, it will not work properly. 
