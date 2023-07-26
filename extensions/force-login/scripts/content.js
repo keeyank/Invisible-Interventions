b12174725ad3bc2af2e269221479fc649fbfaa61
Keeyan Kazemi <kazemikeeyan@gmail.com>
2023-07-26 13:39:38 -0700

    request whenever tik tok is active

--- extensions/force-login/scripts/content.js
+++ extensions/force-login/scripts/content.js
@@ -1,20 +1,38 @@
 // HTTP Requests
-fetch('http://127.0.0.1:8000/usage/?user_id=1', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'text/plain'
-  },
-  body: JSON.stringify({})
+function request() {
+    // User id will need to change to the stored user ID for this extension
+    fetch('http://127.0.0.1:8000/usage/?user_id=1', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle the response data
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle any errors
+        console.error(error);
+    });
+}
+
+request();
+let interval_id = setInterval(request, 5000);
+
+window.addEventListener("focus", (event) => {
+    console.log('Setting interval' + interval_id);
+    request();
+    interval_id = setInterval(request, 5000);
 })
-  .then(response => response.json())
-  .then(data => {
-    // Handle the response data
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error(error);
-  });
+
+window.addEventListener("blur", (event) => {
+    console.log('Clearing interval' + interval_id);
+    clearInterval(interval_id);
+})
+
 
 var port = chrome.runtime.connect({name: "clear_cache"});
 // This isn't great - if the browsing data is removed while they are attempting to
