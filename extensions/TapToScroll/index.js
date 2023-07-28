26a08911241cea7ce94328b1988b6007b9b2b408
Timothy J. Baek <timothyjrbeck@gmail.com>
2023-07-28 16:35:40 +0900

    TapToScroll updated

--- extensions/TapToScroll/index.js
+++ extensions/TapToScroll/index.js
@@ -1,115 +1,15 @@
-console.log(`hello from ${window.location.href}`);
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   // listen for messages sent from background.js
   if (request.message === "urlChange") {
     console.log(request.url); // new url is now in content scripts!
-
-    if (request.url === "https://www.instagram.com/") {
-      setup();
-    } else {
-      destroy();
-    }
+    window.location.reload();
   }
 });
 
-const moveToPreviousPost = () => {
-  let selectedArticleIdx = 0;
-
-  // Iterate through articles until an unviewed one is found.
-  for (let idx = 0; idx < articles.length; idx++) {
-    if (articles[idx].getAttribute("viewed") && idx > selectedArticleIdx) {
-      selectedArticleIdx = idx;
-    }
-  }
-
-  selectedArticleIdx -= 1;
-
-  if (selectedArticleIdx >= 0) {
-    let article = articles[selectedArticleIdx];
-    article.scrollIntoView({ block: "center" });
-
-    // for the remounted posts above
-    for (let idx = 0; idx < selectedArticleIdx; idx++) {
-      articles[idx].setAttribute("viewed", "true");
-    }
-    articles[selectedArticleIdx + 1].removeAttribute("viewed");
-  } else {
-    alert(
-      "You've reached the top of the page. No more content is available above this point."
-    );
-  }
+var s = document.createElement("script");
+s.src = chrome.runtime.getURL("script.js");
+s.onload = function () {
+  this.remove();
 };
-
-const moveToNextPost = () => {
-  let selectedArticleIdx = 0;
-
-  // Iterate through articles until an unviewed one is found.
-  for (let idx = 0; idx < articles.length; idx++) {
-    if (articles[idx].getAttribute("viewed") && idx > selectedArticleIdx) {
-      selectedArticleIdx = idx;
-    }
-  }
-
-  if (selectedArticleIdx !== articles.length - 1) {
-    if (selectedArticleIdx === 0) {
-      articles[selectedArticleIdx].setAttribute("viewed", "true");
-    }
-
-    let article = articles[selectedArticleIdx + 1];
-    article.scrollIntoView({ block: "center" });
-    article.setAttribute("viewed", "true");
-  } else {
-    alert("The next post is being loaded. Please wait a moment and try again.");
-  }
-};
-
-// Retrieves all articles currently loaded in the DOM.
-let articles = document.getElementsByTagName("article");
-
-const ButtonGroupElement = document.createElement("div");
-ButtonGroupElement.style.cssText =
-  "position:fixed;width:100vw;height:50px;bottom:50px;z-index:999999;background:black;display: flex;justify-content: center; align-items: center;text-align: center; border: 1px gray; border-style: solid none;";
-
-// Adds a button above the existing buttons to enable tap-to-scroll functionality.
-// When clicked, the "taptoscroll" function is invoked.
-const PreviousButtonElement = document.createElement("div");
-PreviousButtonElement.style.cssText =
-  "width:50vw; height: 100%;background:rgb(250, 250, 250); color: black; display: flex;justify-content: center; align-items: center;text-align: center; border-right: solid 0.5px gray;";
-PreviousButtonElement.innerHTML += "Previous";
-PreviousButtonElement.onclick = () => {
-  moveToPreviousPost();
-};
-
-const NextButtonElement = document.createElement("div");
-NextButtonElement.style.cssText =
-  "width:50vw; height: 100%;background:rgb(250, 250, 250);color: black;display: flex;justify-content: center; align-items: center;text-align: center; border-left: solid 0.5px gray;";
-NextButtonElement.innerHTML += "Next";
-NextButtonElement.onclick = () => {
-  moveToNextPost();
-};
-
-ButtonGroupElement.appendChild(PreviousButtonElement);
-ButtonGroupElement.appendChild(NextButtonElement);
-
-const setup = () => {
-  // Disables scrolling by adding the "touch-action: none" CSS property to the main element.
-  // This allows tapping on comments and other interactive elements while preventing scrolling.
-  const main = document.getElementsByTagName("main")[0];
-  main.style.cssText = "touch-action: none;";
-
-  // Implements a scroll-to-next-post functionality upon tapping.
-  // This is necessary due to the dynamic addition and removal of article DOM objects.
-  // To ensure that each post is displayed to the user only once, an attribute is set and checked.
-  // If the attribute is absent, the next article is selected and shown.
-
-  document.body.insertBefore(ButtonGroupElement, document.body.firstChild);
-};
-
-const destroy = () => {
-  document.body.remove(ButtonGroupElement);
-};
-
-if (window.location.href === "https://www.instagram.com/") {
-  setup();
-}
+// see also "Dynamic values in the injected code" section in this answer
+(document.head || document.documentElement).appendChild(s);
---
+++ extensions/TapToScroll/script.js
@@ -0,0 +1,57 @@
+const urls = [
+  "https://www.tiktok.com/",
+  "https://www.tiktok.com/foryou",
+  "https://www.tiktok.com/following",
+  "",
+];
+
+let interval = setInterval(() => {
+  let swiperElement = document.getElementsByClassName("swiper");
+  console.log(swiperElement);
+
+  if (swiperElement.length > 0) {
+    console.log("a", swiperElement.length);
+    console.log("a", swiperElement[0]);
+    console.log("a", Object.getOwnPropertyNames(swiperElement[0]));
+
+    if (urls.includes(window.location.origin + window.location.pathname)) {
+      console.log(
+        `hello from ${window.location.origin + window.location.pathname}`
+      );
+
+      const ButtonGroupElement = document.createElement("div");
+      ButtonGroupElement.style.cssText =
+        "position:fixed;width:100vw;height:50px;bottom:50px;z-index:999999;background:black;display: flex;justify-content: center; align-items: center;text-align: center; border: 1px gray; border-style: solid none;";
+
+      // Adds a button above the existing buttons to enable tap-to-scroll functionality.
+      // When clicked, the "taptoscroll" function is invoked.
+      const PreviousButtonElement = document.createElement("div");
+      PreviousButtonElement.style.cssText =
+        "width:50vw; height: 100%;background:rgb(250, 250, 250); color: black; display: flex;justify-content: center; align-items: center;text-align: center; border-right: solid 0.5px gray;";
+      PreviousButtonElement.innerHTML += "Previous";
+
+      const NextButtonElement = document.createElement("div");
+      NextButtonElement.style.cssText =
+        "width:50vw; height: 100%;background:rgb(250, 250, 250);color: black;display: flex;justify-content: center; align-items: center;text-align: center; border-left: solid 0.5px gray;";
+      NextButtonElement.innerHTML += "Next";
+
+      ButtonGroupElement.appendChild(PreviousButtonElement);
+      ButtonGroupElement.appendChild(NextButtonElement);
+
+      console.log(swiperElement);
+
+      let swiper = swiperElement[0].swiper;
+      swiper.allowTouchMove = false;
+
+      PreviousButtonElement.onclick = () => {
+        swiper.slidePrev();
+      };
+      NextButtonElement.onclick = () => {
+        swiper.slideNext();
+      };
+      document.body.insertBefore(ButtonGroupElement, document.body.firstChild);
+    }
+
+    clearInterval(interval);
+  }
+}, 100);
