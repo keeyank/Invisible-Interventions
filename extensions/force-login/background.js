a75e04daeab5658d79a83115b60a1e65ddd53dd5
Keeyan Kazemi <kazemikeeyan@gmail.com>
2023-07-30 18:07:39 -0700

    Attempts to fix bug involving early reload

--- extensions/force-login/background.js
+++ extensions/force-login/background.js
@@ -1,17 +1,16 @@
 // Clear cache specifically for Tik Tok
-let minutesPerDataClear = 0.5;
+let minutesPerDataClear = 1;
 
 function clearCache() {
-
-    var callback = function () {
-        console.log("Browsing data for Tik Tok has been removed");
-    };
+    // var callback = function () {
+    //     console.log("Browsing data for Tik Tok has been removed");
+    // };
     
-    chrome.browsingData.remove({
-        "origins": ["https://www.tiktok.com/"]
-    }, {
-        "cookies": true
-    }, callback);
+    // chrome.browsingData.remove({
+    //     "origins": ["https://www.tiktok.com/"]
+    // }, {
+    //     "cookies": true
+    // }, callback);
 }
 
 clearCache();
--- extensions/force-login/scripts/content.js
+++ extensions/force-login/scripts/content.js
@@ -35,7 +35,19 @@ window.addEventListener("blur", (event) => {
 
 // TODO: Only have the reload occur when already logged in
 window.addEventListener("blur", (event) => {
-    location.reload()
+    // TODO: This doesn't work, update it so the cookies are done in the background.js
+    // and make it so the await syntax is correct. And you can delete the anonymous function
+    // as it is just for debugging. 
+    // I actually almostn guarantee this will not work. It is likely that the cookies will
+    // have been populated at this point. We need to see what cookie in particular we need
+    // to check for. We can log the cookies to the console to see what cookies there will
+    // be at that point, and what cookies we need to check for.
+    cookies = await chrome.cookies.getAll({ domain: 'tiktok.com' }, function(cookies) {
+        console.log(cookies);
+    });
+    if (cookies.length !== 0) {
+        location.reload()
+    }
 });
 
 // var port = chrome.runtime.connect({name: "clear_cache"});
