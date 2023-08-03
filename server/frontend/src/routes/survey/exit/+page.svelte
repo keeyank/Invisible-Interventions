7046886c2c02adbf377f09b5cd1c003bc5855ff3
Timothy J. Baek <timothyjrbeck@gmail.com>
2023-08-03 15:44:38 +0900

    exit survey frontend updated

--- server/frontend/src/routes/survey/exit/+page.svelte
+++ server/frontend/src/routes/survey/exit/+page.svelte
@@ -481,7 +481,7 @@
 						<input
 							type="radio"
 							bind:group={formData.intervention_effect}
-							value={'5'}
+							value={'7'}
 							class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
 						/>
 						<div class="ml-2 text-sm font-medium text-gray-700">
