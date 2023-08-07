7fdc27eba1b4172f0fab33ba412e78512b9bf005
Timothy J. Baek <timothyjrbeck@gmail.com>
2023-08-07 14:09:44 +0900

    frontend updated

--- server/frontend/src/routes/+page.svelte
+++ server/frontend/src/routes/+page.svelte
@@ -1,6 +1,27 @@
+<script>
+	import toast from 'svelte-french-toast';
+
+	const downloadExtension = (name = 'force-login') => {
+		window.open(
+			`https://github.com/keeyank/Invisible-Interventions/raw/main/extensions/_zips/${name}.zip`,
+			'_self'
+		);
+
+		toast.success(
+			'Add the extension by tapping on the three vertical dots in the top-right corner to open the menu and selecting "Extensions".',
+			{
+				duration: 10000
+			}
+		);
+	};
+</script>
+
 <svelte:head>
 	<title>Invisible Interventions | SFU</title>
 </svelte:head>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 
 <div class="mx-4 mb-20">
 	<div class="">
@@ -20,49 +41,37 @@
 		<div
 			class="mb-3 text-sm text-center font-semibold w-full rounded-lg py-4 px-5 text-red-800 bg-green-100 hover:bg-green-300 transition-all cursor-pointer"
 			on:click={() => {
-				window.open(
-					'https://github.com/keeyank/Invisible-Interventions/raw/main/extensions/_zips/force-login.zip',
-					'_self'
-				);
+				downloadExtension('force-login');
 			}}
 		>
-			Force Login Intervention
+			Download Force Login Intervention
 		</div>
 
 		<div
 			class="mb-3 text-sm text-center font-semibold w-full rounded-lg py-4 px-5 text-red-800 bg-green-100 hover:bg-green-300 transition-all cursor-pointer"
 			on:click={() => {
-				window.open(
-					'https://github.com/keeyank/Invisible-Interventions/raw/main/extensions/_zips/gradual-grayscale.zip',
-					'_self'
-				);
+				downloadExtension('gradual-grayscale');
 			}}
 		>
-			Gradual Grayscale Intervention
+			Download Gradual Grayscale Intervention
 		</div>
 
 		<div
 			class="mb-3 text-sm text-center font-semibold w-full rounded-lg py-4 px-5 text-red-800 bg-green-100 hover:bg-green-300 transition-all cursor-pointer"
 			on:click={() => {
-				window.open(
-					'https://github.com/keeyank/Invisible-Interventions/raw/main/extensions/_zips/tap-to-scroll.zip',
-					'_self'
-				);
+				downloadExtension('tap-to-scroll');
 			}}
 		>
-			Tap To Scroll Intervention
+			Download Tap To Scroll Intervention
 		</div>
 
 		<div
 			class="mb-3 text-sm text-center font-semibold w-full rounded-lg py-4 px-5 text-red-800 bg-green-100 hover:bg-green-300 transition-all cursor-pointer"
 			on:click={() => {
-				window.open(
-					'https://github.com/keeyank/Invisible-Interventions/raw/main/extensions/_zips/time-limit.zip',
-					'_self'
-				);
+				downloadExtension('time-limit');
 			}}
 		>
-			Time Limit Intervention
+			Download Time Limit Intervention
 		</div>
 	</div>
 </div>
